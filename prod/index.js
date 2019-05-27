d8464f83bd02f91ace6b5561024f75ffc95991f9
Taran Vohra <taran.vohra@hotmail.com>
2019-05-27 22:13:23 +0530

    add babel & prettier config

---
+++ prod/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _discord = _interopRequireDefault(require("discord.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var x = 22;
+console.log(22);
