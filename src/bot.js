a4d0f2af6bd9299bfeed8ae849e128e468d24377
Taran Vohra <taran.vohra@hotmail.com>
2019-05-30 23:22:05 +0530

    initial commands for bot

--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,35 @@
 import { Client } from 'discord.js';
 import dotenv from 'dotenv';
+import { prefix, commands } from './contants';
 dotenv.config();
+
+const bBot = new Client({
+  disabledEvents: ['TYPING_START', 'CHANNEL_UPDATE', 'USER_UPDATE'],
+});
+
+bBot.on('message', async message => {
+  if (message.author.equals(bot.user)) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const { roles } = message.memeber;
+  const {
+    channel: {
+      guild: { id: serverId },
+    },
+  } = message;
+  const [first, ...args] = message.content
+    .substring(prefix.length)
+    .split(' ')
+    .filter(Boolean);
+  const action = first && first.toLowerCase();
+
+  const foundCommand = commands.find(cmd => cmd.aliases.includes(action));
+});
 
 /*
  * BOT
  *  INITIALIZATION
  */
-const bBot = new Client({
-  disabledEvents: ['TYPING_START', 'CHANNEL_UPDATE', 'USER_UPDATE'],
-});
-
 (() => {
   bBot.login(process.env.DISCORD_BOT_TOKEN);
 })();
---
+++ src/contants.js
@@ -0,0 +1,24 @@
+export const prefix = '.';
+
+export const commands = [
+  {
+    key: 'addQueryServer',
+    description: '',
+    aliases: ['addqueryserver'],
+  },
+  {
+    key: 'delQueryServer',
+    description: '',
+    aliases: ['delqueryserver'],
+  },
+  {
+    key: 'queryUT99Server',
+    description: '',
+    aliases: ['q', 'query'],
+  },
+  {
+    key: 'servers',
+    description: '',
+    aliases: ['servers'],
+  },
+];
